72d1ebe1980d4a13dae7291b880e1ed73c47f0f7
leonardomurakami <leonardo.murakami@loft.com.br>
2026-03-12 13:52:17 -0300

    [feat] improve boilerplate

--- app/main.py
+++ app/main.py
@@ -8,8 +8,7 @@ def home():
     return {"message": "Health check for now"}
 
 def start():
-    """Launch application with uvicorn"""
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
 
 if __name__ == "__main__":
     start()
